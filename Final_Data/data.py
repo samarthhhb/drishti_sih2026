01e3f8e80c435698445a238ab45c04ea9f0bb869
Samarth Buchake <samarth0607b@gmail.com>
2026-08-28 17:15:29 +0530

    Dataset Finalisation.

---
+++ Final_Data/data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+paths = ["/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 1A/Breakdown.csv", 
+         "/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 1B/Breakdown.csv",
+         "/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 1C/Breakdown.csv",
+         "/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 1D/Breakdown.csv",
+         "/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 1E/Breakdown.csv",
+         "/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 1J/Breakdown.csv",
+         "/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 1K/Breakdown.csv",
+         "/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 1L/Breakdown.csv",
+         "/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 8J/Breakdown.csv",
+         "/Users/samarth/Documents/SIH 26/Dataset/IGBT-IRG4BC30K/Part 8L/Breakdown.csv"]
